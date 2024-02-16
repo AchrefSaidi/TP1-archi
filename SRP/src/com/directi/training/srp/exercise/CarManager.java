e667969bbed751ff112662c661adc220d8269c54
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-16 11:25:54 +0100

    Update CarManager.java

--- SRP/src/com/directi/training/srp/exercise/CarManager.java
+++ SRP/src/com/directi/training/srp/exercise/CarManager.java
@@ -1,44 +1,22 @@
 package com.directi.training.srp.exercise;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class CarManager
-{
-    private List<Car> _carsDb = Arrays
-        .asList(new Car("1", "Golf III", "Volkswagen"), new Car("2", "Multipla", "Fiat"),
-            new Car("3", "Megane", "Renault"));
+public class CarManager {
+    private CarDatabase _carDatabase;
+    private CarFormatter _carFormatter;
 
-    public Car getFromDb(final String carId)
-    {
-        for (Car car : _carsDb) {
-            if (car.getId().equals(carId)) {
-                return car;
-            }
-        }
-        return null;
+    public CarManager(CarDatabase carDatabase, CarFormatter carFormatter) {
+        _carDatabase = carDatabase;
+        _carFormatter = carFormatter;
     }
 
-    public String getCarsNames()
-    {
-        StringBuilder sb = new StringBuilder();
-        for (Car car : _carsDb) {
-            sb.append(car.getBrand());
-            sb.append(" ");
-            sb.append(car.getModel());
-            sb.append(", ");
-        }
-        return sb.substring(0, sb.length() - 2);
+    public List<Car> getAllCars() {
+        return _carDatabase.getAllCars();
     }
 
-    public Car getBestCar()
-    {
-        Car bestCar = null;
-        for (Car car : _carsDb) {
-            if (bestCar == null || car.getModel().compareTo(bestCar.getModel()) > 0) {
-                bestCar = car;
-            }
-        }
-        return bestCar;
+    public String getCarsNames() {
+        List<Car> allCars = _carDatabase.getAllCars();
+        return _carFormatter.formatCarNames(allCars);
     }
 }
