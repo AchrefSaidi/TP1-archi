762856a1d775679163cdda42207dd5e23b2ec67f
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-16 11:35:27 +0100

    Update CarManager.java

--- SRP/src/com/directi/training/srp/exercise/CarManager.java
+++ SRP/src/com/directi/training/srp/exercise/CarManager.java
@@ -3,20 +3,20 @@ package com.directi.training.srp.exercise;
 import java.util.List;
 
 public class CarManager {
-    private CarDatabase _carDatabase;
+    private CarDb _carDatabase;
     private CarFormatter _carFormatter;
 
-    public CarManager(CarDatabase carDatabase, CarFormatter carFormatter) {
+    public CarManager(CarDb carDatabase, CarFormatter carFormatter) {
         _carDatabase = carDatabase;
         _carFormatter = carFormatter;
     }
 
     public List<Car> getAllCars() {
-        return _carDatabase.getAllCars();
+        return _carDatabase.getCars();
     }
 
     public String getCarsNames() {
-        List<Car> allCars = _carDatabase.getAllCars();
-        return _carFormatter.formatCarNames(allCars);
+        List<Car> allCars = _carDatabase.getCars();
+        return _carFormatter.getCarNames(allCars);
     }
 }
