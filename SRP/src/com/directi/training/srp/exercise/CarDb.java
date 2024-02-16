85557d960910a61749da09d65b5b81b5495cc710
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-16 11:27:35 +0100

    Create CarDb.java

---
+++ SRP/src/com/directi/training/srp/exercise/CarDb.java
@@ -0,0 +1,20 @@
+package com.directi.training.srp.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarDatabase {
+    private List<Car> _carsDb;
+
+    public CarDatabase() {
+        _carsDb = Arrays.asList(
+                new Car("1", "Golf III", "Volkswagen"),
+                new Car("2", "Multipla", "Fiat"),
+                new Car("3", "Megane", "Renault")
+        );
+    }
+
+    public List<Car> getAllCars() {
+        return _carsDb;
+    }
+}
