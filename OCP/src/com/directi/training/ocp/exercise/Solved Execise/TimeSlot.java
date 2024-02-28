7b48d89737a070f8624e0b5648aebe36b8f77d2c
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-28 18:37:46 +0100

    Create TimeSlot.java

---
+++ OCP/src/com/directi/training/ocp/exercise/Solved Execise/TimeSlot.java
@@ -0,0 +1,19 @@
+package com.directi.training.ocp.exercise;
+
+public class TimeSlot implements Resource {
+    @Override
+    public void markFree(int resourceId) {
+        
+    }
+
+    @Override
+    public void markBusy(int resourceId) {
+        
+    }
+
+    @Override
+    public int findFreeSlot() {
+        
+        return 0;
+    }
+}
