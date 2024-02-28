dfb2f9edeaef620f30904fad6ed0034a9dce1c63
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-28 18:33:46 +0100

    Update ResourceAllocator.java

--- OCP/src/com/directi/training/ocp/exercise/Solved Execise/ResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise/Solved Execise/ResourceAllocator.java
@@ -1,1 +1,16 @@
+package com.directi.training.ocp.exercise;
+
+public class ResourceAllocator {
+    private static final int INVALID_RESOURCE_ID = -1;
+
+    public int allocate(Resource resource) {
+        int resourceId = resource.findFreeSlot();
+        resource.markBusy(resourceId);
+        return resourceId;
+    }
+
+    public void free(Resource resource, int resourceId) {
+        resource.markFree(resourceId);
+    }
+}
 
