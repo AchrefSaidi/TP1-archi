bef4bd048e70534877eaba2d96b81f64b97a8782
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-29 18:22:57 +0100

    Create GeneralEncodingModule.java

---
+++ DIP/src/com/directi/training/dip/exercise/Solved exercise/GeneralEncodingModule.java
@@ -0,0 +1,9 @@
+package com.directi.training.dip.exercise.solution;
+
+import java.io.IOException;
+
+public interface GeneralEncodingModule {
+    public void encodeWithFiles()  throws IOException;
+    public void encodeBasedOnNetworkAndDatabase()  throws IOException;
+}
+
