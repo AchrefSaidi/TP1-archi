5ee400179b465ab3bef96121993d90c5d352b0f2
Achref Saidi <81881745+AchrefSaidi@users.noreply.github.com>
2024-02-29 12:18:13 +0100

    Create Pool.java

---
+++ LSP/src/com/directi/training/lsp/exercise/Solved exercise/Pool.java
@@ -0,0 +1,39 @@
+package com.directi.training.lsp.exercise.solution;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pool
+{
+    public void run()
+    {
+        List<IDuck> ducks = new ArrayList<IDuck>();
+        ducks.add(new Duck());
+        ducks.add(new ElectronicDuck());
+        
+        quack(ducks);
+        swim(ducks);
+    }
+
+    private void quack(List<IDuck> ducks )
+    {
+        for (IDuck duck : ducks) {
+            duck.quack();
+        }
+    }
+
+    private void swim(List<IDuck> ducks )
+    {
+        for (IDuck duck : ducks) {
+            if (duck instanceof ElectronicDuck) {
+                ((ElectronicDuck)duck).turnOn();
+            }
+            duck.swim();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Pool pool = new Pool();
+        pool.run();
+    }
+}
